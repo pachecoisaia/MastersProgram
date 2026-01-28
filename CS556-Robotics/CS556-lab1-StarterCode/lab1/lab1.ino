2195eabfd22e8178c71b71f1c79cd94d6efd6875
Isaia Pacheco <ipacheco9311@sdsu.edu>
2026-01-27 16:02:08 -0800

    adding cs 556 robotics spring 2026

---
+++ CS556-Robotics/CS556-lab1-StarterCode/lab1/lab1.ino
@@ -0,0 +1,28 @@
+#include <Pololu3piPlus32U4.h>
+#include "my_robot.h"
+using namespace Pololu3piPlus32U4;
+
+void setup() {
+  Serial.begin(9600);
+  delay(15);
+}
+
+void loop(){
+    //Sets the speeds for motors 
+    //The value of the input is given in mm/s with a maximum value of 400
+    
+    //move forward
+
+    //move backwards
+
+    //turn right(in place)
+
+    //turn left (in place)
+
+    //move forward while turning right
+
+    //move forward while turning left
+
+
+    //halt
+}
---
+++ CS556-Robotics/CS556-lab1-StarterCode/lab1/my_robot.h
@@ -0,0 +1,14 @@
+#ifndef my_robot_h
+#define my_robot_h
+#include <Pololu3piPlus32U4.h>
+using namespace Pololu3piPlus32U4;
+
+class MyRobot{
+  public:
+    MyRobot();
+    
+  private:
+    
+};
+
+#endif
